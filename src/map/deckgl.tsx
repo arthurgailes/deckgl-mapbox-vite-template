a494fad231b9848e0c9cb07650cf44a813868f6b
Arthur Gailes <agailes1@gmail.com>
2022-05-17 14:48:40 -0400

    update for env variables

--- src/map/deckgl.tsx
+++ src/map/deckgl.tsx
@@ -1,11 +1,11 @@
 // import React from 'react';
-import DeckGL from "@deck.gl/react";
-import { useState, useCallback, useRef, useEffect } from "react";
+import DeckGL from '@deck.gl/react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 
-import { MapboxLayer } from "@deck.gl/mapbox";
-import { MapboxAddons, basemaps } from "./MapboxAddons";
-import { returnTooltip } from "./tooltip";
-import { StaticMap, _MapContext as MapContext } from "react-map-gl";
+import { MapboxLayer } from '@deck.gl/mapbox';
+import { MapboxAddons, basemaps } from './MapboxAddons';
+import { returnTooltip } from './tooltip';
+import { StaticMap, _MapContext as MapContext } from 'react-map-gl';
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
@@ -16,19 +16,12 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
 };
 
-export const Map = function ({
-  viewState,
-  setViewState,
-  appRef,
-  layers,
-  mapRef,
-}: any) {
+export const Map = function ({ viewState, setViewState, appRef, layers, mapRef }: any) {
   useEffect(() => setViewState(INITIAL_VIEW_STATE), []);
   // DeckGL and mapbox will both draw into this WebGL context
-
   const setViewStateWithObject = useCallback(
     ({ viewState }) => setViewState(viewState),
-    []
+    [],
   );
 
   const [glContext, setGLContext] = useState();
@@ -39,15 +32,13 @@ export const Map = function ({
     const { deck } = deckRef.current;
 
     // get the id of the first label layer
-    const showLabel = map
-      .getStyle()
-      .layers.find((l: any) => l.type === "symbol").id;
+    const showLabel = map.getStyle().layers.find((l: any) => l.type === 'symbol').id;
 
     // You must initialize an empty deck.gl layer to prevent flashing
     map.addLayer(
       // This id has to match the id of the deck.gl layer
-      new MapboxLayer({ id: "deckgl", deck }),
-      showLabel
+      new MapboxLayer({ id: 'deckgl', deck }),
+      showLabel,
       // Optionally define id from Mapbox layer stack under which to add deck layer
       // 'before-layer-id',
     );
